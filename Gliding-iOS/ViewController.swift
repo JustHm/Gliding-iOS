69cbf0483a6511017c25717fca10de162a1a10fb
JustHm <taggma12@gmail.com>
2025-02-06 13:32:55 +0900

    Initial Commit

---
+++ Gliding-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gliding-iOS
+//
+//  Created by 안정흠 on 2/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
